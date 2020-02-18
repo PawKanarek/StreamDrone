c8164f30e650ac901f2044675a28d6370c26f6b7
DESKTOP-SQK3IRP\Raix <paw.kanarek@gmail.com>
2020-02-18 21:22:39 +0100

    zmiana adresu http

--- src/app/src/main/java/pl/raczek/streamdrone/FullscreenActivity.kt
+++ src/app/src/main/java/pl/raczek/streamdrone/FullscreenActivity.kt
@@ -12,35 +12,23 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 class FullscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fullscreen)
 
-
         val playerView = this.findViewById<PlayerView>(R.id.exo_player_view)
         val player = SimpleExoPlayer.Builder(this).build()
-        playerView.player = player;
+        playerView.player = player
 
-
-        // Produces DataSource instances through which media data is loaded.
-        // Produces DataSource instances through which media data is loaded.
         val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
             this,
             Util.getUserAgent(this, "yourApplicationName")
         )
 
-        var uri = Uri.parse("https://75.149.107.121:8081/mjpg/video.mjpg");
-        // This is the MediaSource representing the media to be played.
-        // This is the MediaSource representing the media to be played.
+        var uri = Uri.parse("192.168.0.56:8081")
         val videoSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
             .createMediaSource(uri)
-        // Prepare the player with the source.
-        // Prepare the player with the source.
         player.prepare(videoSource)
     }
 }
